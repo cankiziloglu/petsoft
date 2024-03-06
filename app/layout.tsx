ae8f68983529b3f7de5e23446d48f672ec771720
Can Gokhan Kiziloglu <can.kiziloglu@gmail.com>
2024-03-06 16:12:34 +0300

    set up overall styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "PetSoft Pet Daycare Software",
-  description: "PetSoft is the best pet daycare software for your business.",
+  title: 'PetSoft Pet Daycare Software',
+  description: 'PetSoft is the best pet daycare software for your business.',
 };
 
 export default function RootLayout({
@@ -15,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body
+        className={`${inter.className} bg-slate-50 text-gray-900 text-sm min-h-dvh`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
